602c7ee8815a4908bfca63fbe7ae476d21e7d963
André Maree <andrem@kss.co.za>
2020-03-24 19:38:38 +0200

    Added lipstick to [IF_][C]PRINT/TRACK macros

--- x_printf.h
+++ x_printf.h
@@ -45,15 +45,15 @@ extern uint64_t RunTime ;
 
 #define	PRINT(f, ...)					printfx(f, ##__VA_ARGS__)
 #define	TRACK(f, ...)					printfx(_TRACK_(f), ##__VA_ARGS__)
-#define	IF_PRINT(T,f, ...)				if (T) PRINT(f, ##__VA_ARGS__)
-#define	IF_TRACK(T,f, ...)				if (T) TRACK(f,##__VA_ARGS__)
+#define	IF_PRINT(T, f, ...)				if (T) PRINT(f, ##__VA_ARGS__)
+#define	IF_TRACK(T, f, ...)				if (T) TRACK(f, ##__VA_ARGS__)
 
 /* The direct output functions must ONLY be used to debug tasks that work with redirected
  * STDOUT such as the HTTP and TelNET tasks */
 #define	CPRINT(f, ...)					cprintfx(f, ##__VA_ARGS__)
 #define	CTRACK(f, ...)					cprintfx(_TRACK_(f), ##__VA_ARGS__)
-#define	IF_CPRINT(T,f, ...)				if (T) CPRINT(f, ##__VA_ARGS__)
-#define	IF_CTRACK(T,f, ...)				if (T) CTRACK(f,##__VA_ARGS__)
+#define	IF_CPRINT(T, f, ...)			if (T) CPRINT(f, ##__VA_ARGS__)
+#define	IF_CTRACK(T, f, ...)			if (T) CTRACK(f, ##__VA_ARGS__)
 
 // ###################### control functionality included in xprintf.c ##############################
 
