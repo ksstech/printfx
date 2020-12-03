350256c3a157e434ec65313fb55aa84354a3c214
André Maree <ammaree@users.noreply.github.com>
2020-12-03 16:25:38 +0200

    Fix and add tests

--- printfx.c
+++ printfx.c
@@ -1090,6 +1090,12 @@ int		PrintFX(int (handler)(xpc_t *, int), void * pVoid, size_t BufSize, const ch
 #if		(xpfSUPPORT_DATETIME == 1)
 			case CHR_Z:								// DATE, TIME, ZONE
 				vPrintDateTimeZone(&sXPC, va_arg(vArgs, TSZ_t *)) ;		// para =  SysTime
+			case CHR_r:				// U32->U64 epoch (yr+mth+day) or relative (days) + TIME
+				IF_myASSERT(debugTRACK, sXPC.f.alt_form == 0 && sXPC.f.plus == 0 &&
+										sXPC.f.pad0 == 0 && sXPC.f.radix == 0 &&
+										sXPC.f.group == 0) ;
+				sXPC.f.pad0		= 1 ;
+				vPrintDateTime(&sXPC, (uint64_t) va_arg(vArgs, uint32_t) * MILLION) ;
 				break ;
 #endif
 
@@ -1171,8 +1177,6 @@ int		PrintFX(int (handler)(xpc_t *, int), void * pVoid, size_t BufSize, const ch
 #endif
 
 #if		(xpfSUPPORT_DATETIME == 1)
-			case CHR_r:								// u32 timestamp (abs or rel) -> u64 uSecs
-				vPrintDateTimeUSec(&sXPC, (uint64_t) va_arg(vArgs, uint32_t) * MILLION) ;
 				break ;
 #endif
 
