6d7a8707cb222db48073776fc7163731f9a4eb23
André Maree <ammaree@users.noreply.github.com>
2021-01-14 11:01:08 +0200

    Update to use existing ANSI attribute functionality

--- formprint.c
+++ formprint.c
@@ -5,9 +5,12 @@
  *
  */
 
+#include	"hal_config.h"
 #include 	"formprint.h"
 #include	"printfx.h"
 #include	"x_terminal.h"
+
+#define	formMASK(f,m,p,a)	(((f & m) >> p) + a)
 
 /*
  *	vLogPrePend - process all options to prepend info
@@ -17,22 +20,10 @@
  *	\return	none
  */
 void	vLogPrePend(uint32_t flag) {
-// Handle foreground colour
-	if (flag & FLAG_CB_FG_MASK) {
-		vTermSetForeground((flag & FLAG_CB_FG_MASK) >> 29) ;
-	}
-// Handle background colour
-	if (flag & FLAG_CB_BG_MASK) {
-		vTermSetBackground((flag & FLAG_CB_BG_MASK) >> 25) ;
-	}
-// Insert the PREpend options
-    if (flag & FLAG_CB_PRE_CL) {
-    	PRINT("%c", CHR_CR) ;
-    	PRINT("%c", CHR_LF) ;
-    }
-    if (flag & FLAG_CB_PRE_SPC) {
-    	PRINT("%c", CHR_SPACE) ;
-    }
+	if (flag & FLAG_CB_FG_MASK)		vANSIattrib(formMASK(flag, FLAG_CB_FG_MASK, 29, 30), 0) ;
+	if (flag & FLAG_CB_BG_MASK)		vANSIattrib(formMASK(flag, FLAG_CB_BG_MASK, 25, 40), 0) ;
+    if (flag & FLAG_CB_PRE_CL)		putcx(CHR_LF, configSTDIO_UART_CHAN) ;
+    if (flag & FLAG_CB_PRE_SPC)    	putcx(CHR_SPACE, configSTDIO_UART_CHAN) ;
 }
 
 /*
@@ -43,61 +34,29 @@ void	vLogPrePend(uint32_t flag) {
  *	\return	none
  */
 void	vLogPostPend(uint32_t flag) {
-// Reset fore/background colours
-	if (flag & FLAG_CB_FG_MASK) {
-		vTermSetForeground(FLAG_CB_FG_WHITE >> 29) ;
-	}
-	if (flag & FLAG_CB_BG_MASK) {
-		vTermSetBackground(FLAG_CB_BG_BLACK >> 25) ;
-	}
-// Insert the POSTpend options
-    if (flag & FLAG_CB_POST_COMMA) {
-    	PRINT("%c", CHR_COMMA) ;
-    }
-    if (flag & FLAG_CB_POST_SPC) {
-    	PRINT("%c", CHR_SPACE) ;
-    }
-    if (flag & FLAG_CB_POST_TAB) {
-    	PRINT("%c", CHR_TAB) ;
-    }
-    if (flag & FLAG_CB_POST_CL) {
-    	PRINT("%c", CHR_CR) ;
-    	PRINT("%c", CHR_LF) ;
-    }
+	if (flag & FLAG_CB_FG_MASK)		vANSIattrib(formMASK(FLAG_CB_FG_WHITE, FLAG_CB_FG_MASK, 29, 30), 0) ;
+	if (flag & FLAG_CB_BG_MASK)		vANSIattrib(formMASK(FLAG_CB_BG_BLACK, FLAG_CB_BG_MASK, 25, 40), 0) ;
+    if (flag & FLAG_CB_POST_COMMA)	putcx(CHR_COMMA, configSTDIO_UART_CHAN) ;
+    if (flag & FLAG_CB_POST_SPC)	putcx(CHR_SPACE, configSTDIO_UART_CHAN) ;
+    if (flag & FLAG_CB_POST_TAB)	putcx(CHR_TAB, configSTDIO_UART_CHAN) ;
+    if (flag & FLAG_CB_POST_CL)		putcx(CHR_LF, configSTDIO_UART_CHAN) ;
 }
 
-/*
- * vLogPrintf - write printf format output to the circular buffer
- * \brief	buffer and control structure MUST have been created prior
- * \param	pCB - pointer to circular buffer control structure
- * 			flag	- control buffer formatting flag
- * 			format is pointer to the array to be written
- * 			[...] variable number of parameters
- * \return	None
- */
 void 	vLogPrintf(uint32_t flag, const char * format, ...) {
+	if (flag)						vLogPrePend(flag) ;
 	va_list args ;
-	if (flag) {
-		vLogPrePend(flag) ;								// PREpend handling
-	}
-
     va_start(args, format) ;
-    vprintfx(format, args) ;							// output the actual formatted message
+    vprintfx(format, args) ;
     va_end(args) ;
-
-    if (flag) {
-    	vLogPostPend(flag) ;						    // POSTpend handling
-    }
+    if (flag)						vLogPostPend(flag) ;
 }
 
 int32_t	xI8ArrayPrint(const char * pHeader, uint8_t * pArray, int32_t ArraySize) {
 	int32_t iRV = 0 ;
-	if (pHeader) {
+	if (pHeader)
 		iRV += PRINT(pHeader) ;
-	}
-	for (int32_t Idx = 0; Idx < ArraySize; ++Idx) {
+	for (int32_t Idx = 0; Idx < ArraySize; ++Idx)
 		iRV += PRINT("  #%d=%d", Idx, pArray[Idx]) ;
-	}
 	iRV += PRINT("\n") ;
 	return iRV ;
 }
