943feef4736670909ed1e7c63342b4a43dd8b16d
André Maree <ammaree@users.noreply.github.com>
2022-03-22 22:24:12 +0200

    Update printfx.h

--- printfx.h
+++ printfx.h
@@ -44,82 +44,82 @@ extern unsigned long long RunTime ;
 
 // ###################### control functionality included in xprintf.c ##############################
 
-#define	xpfSUPPORT_BINARY				1
-#define	xpfSUPPORT_MAC_ADDR				1
-#define	xpfSUPPORT_IP_ADDR				1
-#define	xpfSUPPORT_HEXDUMP				1
-#define	xpfSUPPORT_POINTER				1
-#define	xpfSUPPORT_DATETIME				1
-#define	xpfSUPPORT_IEEE754				1		// float point support in printfx.c functions
-#define	xpfSUPPORT_SCALING				1		// scale number down by 10^3/10^6/10^9/10^12
-#define	xpfSUPPORT_SGR					1		// Set Graphics Rendition FG & BG colors only
-#define	xpfSUPPORT_URL					1		// URL encoding
+#define	xpfSUPPORT_BINARY			1
+#define	xpfSUPPORT_MAC_ADDR			1
+#define	xpfSUPPORT_IP_ADDR			1
+#define	xpfSUPPORT_HEXDUMP			1
+#define	xpfSUPPORT_POINTER			1
+#define	xpfSUPPORT_DATETIME			1
+#define	xpfSUPPORT_IEEE754			1		// float point support in printfx.c functions
+#define	xpfSUPPORT_SCALING			1		// scale number down by 10^3/10^6/10^9/10^12
+#define	xpfSUPPORT_SGR				1		// Set Graphics Rendition FG & BG colors only
+#define	xpfSUPPORT_URL				1		// URL encoding
 
-#define	xpfMAXIMUM_DECIMALS				15
-#define	xpfDEFAULT_DECIMALS				6
+#define	xpfMAXIMUM_DECIMALS			15
+#define	xpfDEFAULT_DECIMALS			6
 
-#define	xpfMAX_TIME_FRAC				6		// control resolution mS/uS/nS
-#define	xpfDEF_TIME_FRAC				3
+#define	xpfMAX_TIME_FRAC			6		// control resolution mS/uS/nS
+#define	xpfDEF_TIME_FRAC			3
 
 #ifdef ESP_PLATFORM
 		/* Specifically for the ESP-IDF we1`	 * as x[v]fprintf(stdout, format, ...) with locking enabled */
 	#define	xpfSUPPORT_ALIASES			1
 #else
-	#define	xpfSUPPORT_ALIASES			1
+	#define	xpfSUPPORT_ALIASES		1
 #endif
-#define	xpfSUPPORT_FILTER_NUL			1
+#define	xpfSUPPORT_FILTER_NUL		1
 
 // ################################## x[snf]printf() related #######################################
 
-#define	xpfMAX_LEN_TIME					sizeof("12:34:56.654321")
-#define	xpfMAX_LEN_DATE					sizeof("Sun, 10 Sep 2017")
+#define	xpfMAX_LEN_TIME				sizeof("12:34:56.654321")
+#define	xpfMAX_LEN_DATE				sizeof("Sun, 10 Sep 2017")
 
-#define	xpfMAX_LEN_DTZ					(xpfMAX_LEN_DATE + xpfMAX_LEN_TIME + configTIME_MAX_LEN_TZINFO)
+#define	xpfMAX_LEN_DTZ				(xpfMAX_LEN_DATE + xpfMAX_LEN_TIME + configTIME_MAX_LEN_TZINFO)
 
-#define xpfMAX_LEN_X32					sizeof("+4,294,967,295")
-#define xpfMAX_LEN_X64					sizeof("+18,446,744,073,709,551,615")
+#define xpfMAX_LEN_X32				sizeof("+4,294,967,295")
+#define xpfMAX_LEN_X64				sizeof("+18,446,744,073,709,551,615")
 
-#define xpfMAX_LEN_F32					(sizeof("+4,294,967,295.") + xpfMAXIMUM_DECIMALS)
-#define xpfMAX_LEN_F64					(sizeof("+18,446,744,073,709,551,615.") + xpfMAXIMUM_DECIMALS)
+#define xpfMAX_LEN_F32				(sizeof("+4,294,967,295.") + xpfMAXIMUM_DECIMALS)
+#define xpfMAX_LEN_F64				(sizeof("+18,446,744,073,709,551,615.") + xpfMAXIMUM_DECIMALS)
 
-#define	xpfMAX_LEN_B32					sizeof("1010-1010|1010-1010 1010-1010|1010-1010")
-#define	xpfMAX_LEN_B64					(xpfMAX_LEN_B32 * 2)
+#define	xpfMAX_LEN_B32				sizeof("1010-1010|1010-1010 1010-1010|1010-1010")
+#define	xpfMAX_LEN_B64				(xpfMAX_LEN_B32 * 2)
 
-#define	xpfMAX_LEN_IP					sizeof("123.456.789.012")
-#define	xpfMAX_LEN_MAC					sizeof("01:23:45:67:89:ab")
+#define	xpfMAX_LEN_IP				sizeof("123.456.789.012")
+#define	xpfMAX_LEN_MAC				sizeof("01:23:45:67:89:ab")
 
-#define	xpfMAX_LEN_SGR					sizeof("\033[???;???;???;???m\000")
+#define	xpfMAX_LEN_SGR				sizeof("\033[???;???;???;???m\000")
 
-#define	xpfNULL_STRING					"'null'"
+#define	xpfNULL_STRING				"'null'"
 
-#define	xpfFLAGS_NONE					((xpf_t) 0ULL)
+#define	xpfFLAGS_NONE				((xpf_t) 0ULL)
 
 // Used in hexdump to determine size of each value conversion
-#define	xpfSIZING_BYTE					0			// 8 bit byte
-#define	xpfSIZING_SHORT					1			// 16 bit short
-#define	xpfSIZING_WORD					2			// 32 bit word
-#define	xpfSIZING_DWORD					3			// 64 bit long long word
+#define	xpfSIZING_BYTE				0			// 8 bit byte
+#define	xpfSIZING_SHORT				1			// 16 bit short
+#define	xpfSIZING_WORD				2			// 32 bit word
+#define	xpfSIZING_DWORD				3			// 64 bit long long word
 
 /* For HEXDUMP functionality the size is as follows
  * ( 0x12345678 {32 x 3} [ 32 x Char]) = 142 plus some safety = 160 characters max.
  * When done in stages max size about 96 */
-#define	xpfHEXDUMP_WIDTH				32			// number of bytes (as bytes/short/word/llong) in a single row
+#define	xpfHEXDUMP_WIDTH			32			// number of bytes (as bytes/short/word/llong) in a single row
 
 /* Maximum size is determined by bit width of maxlen and curlen fields below */
-#define	xpfMAXLEN_BITS					16			// Number of bits in field(s)
-#define	xpfMAXLEN_MAXVAL				((1 << xpfMAXLEN_BITS) - 1)
+#define	xpfMAXLEN_BITS				16			// Number of bits in field(s)
+#define	xpfMAXLEN_MAXVAL			((1 << xpfMAXLEN_BITS) - 1)
 
-#define	xpfMINWID_BITS					16			// Number of bits in field(s)
-#define	xpfMINWID_MAXVAL				((1 << xpfMINWID_BITS) - 1)
+#define	xpfMINWID_BITS				16			// Number of bits in field(s)
+#define	xpfMINWID_MAXVAL			((1 << xpfMINWID_BITS) - 1)
 
-#define	xpfPRECIS_BITS					16			// Number of bits in field(s)
-#define	xpfPRECIS_MAXVAL				((1 << xpfPRECIS_BITS) - 1)
+#define	xpfPRECIS_BITS				16			// Number of bits in field(s)
+#define	xpfPRECIS_MAXVAL			((1 << xpfPRECIS_BITS) - 1)
 
 /* https://en.wikipedia.org/wiki/ANSI_escape_code#Escape_sequences
  * http://www.termsys.demon.co.uk/vtansi.htm#colors
  */
 
-#define	xpfSGR(a,b,c,d)					(((uint8_t) d << 24) + ((uint8_t) c << 16) + ((uint8_t) b << 8) + (uint8_t) a)
+#define	xpfSGR(a,b,c,d)				(((uint8_t) d << 24) + ((uint8_t) c << 16) + ((uint8_t) b << 8) + (uint8_t) a)
 
 // ####################################### enumerations ############################################
 
