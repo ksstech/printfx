e0a32f34029e387aa7d8e5407c046f3cbb9639a0
André Maree <ammaree@users.noreply.github.com>
2024-03-06 21:56:24 +0200

    Update formprint.c

--- formprint.c
+++ formprint.c
@@ -1,9 +1,4 @@
-/*
- * Copyright 2014-21 AM Maree/KSS Technologies (Pty) Ltd.
- *
- * formprint.c
- *
- */
+// formprint.c - Copyright (c) 2014-24 Andre M. Maree/KSS Technologies (Pty) Ltd.
 
 #include	"hal_config.h"
 #include 	"formprint.h"
@@ -19,11 +14,11 @@
  * 			flag	- control buffer formatting flag
  *	\return	none
  */
-void	vLogPrePend(uint32_t flag) {
-	if (flag & FLAG_CB_FG_MASK)		vANSIattrib(formMASK(flag, FLAG_CB_FG_MASK, 29, 30), 0) ;
-	if (flag & FLAG_CB_BG_MASK)		vANSIattrib(formMASK(flag, FLAG_CB_BG_MASK, 25, 40), 0) ;
     if (flag & FLAG_CB_PRE_CL)		putcharX(CHR_LF, configSTDIO_UART_CHAN) ;
     if (flag & FLAG_CB_PRE_SPC)    	putcharX(CHR_SPACE, configSTDIO_UART_CHAN) ;
+void vLogPrePend(uint32_t flag) {
+	if (flag & FLAG_CB_FG_MASK)		vANSIattrib(formMASK(flag, FLAG_CB_FG_MASK, 29, 30), 0);
+	if (flag & FLAG_CB_BG_MASK)		vANSIattrib(formMASK(flag, FLAG_CB_BG_MASK, 25, 40), 0);
 }
 
 /*
@@ -33,30 +28,29 @@ void	vLogPrePend(uint32_t flag) {
  * 			flag	- control buffer formatting flag
  *	\return	none
  */
-void	vLogPostPend(uint32_t flag) {
-	if (flag & FLAG_CB_FG_MASK)		vANSIattrib(formMASK(FLAG_CB_FG_WHITE, FLAG_CB_FG_MASK, 29, 30), 0) ;
-	if (flag & FLAG_CB_BG_MASK)		vANSIattrib(formMASK(FLAG_CB_BG_BLACK, FLAG_CB_BG_MASK, 25, 40), 0) ;
     if (flag & FLAG_CB_POST_COMMA)	putcharX(CHR_COMMA, configSTDIO_UART_CHAN) ;
     if (flag & FLAG_CB_POST_SPC)	putcharX(CHR_SPACE, configSTDIO_UART_CHAN) ;
     if (flag & FLAG_CB_POST_TAB)	putcharX(CHR_TAB, configSTDIO_UART_CHAN) ;
     if (flag & FLAG_CB_POST_CL)		putcharX(CHR_LF, configSTDIO_UART_CHAN) ;
+void vLogPostPend(u32_t flag) {
+	if (flag & FLAG_CB_FG_MASK)		vANSIattrib(formMASK(FLAG_CB_FG_WHITE, FLAG_CB_FG_MASK, 29, 30), 0);
+	if (flag & FLAG_CB_BG_MASK)		vANSIattrib(formMASK(FLAG_CB_BG_BLACK, FLAG_CB_BG_MASK, 25, 40), 0);
 }
 
-void 	vLogPrintf(uint32_t flag, const char * format, ...) {
-	if (flag)						vLogPrePend(flag) ;
-	va_list args ;
-    va_start(args, format) ;
-    vprintfx(format, args) ;
-    va_end(args) ;
-    if (flag)						vLogPostPend(flag) ;
+void vLogPrintf(u32_t flag, const char * format, ...) {
+	if (flag) vLogPrePend(flag);
+	va_list args;
+    va_start(args, format);
+    vprintfx(format, args);
+    va_end(args);
+    if (flag) vLogPostPend(flag);
 }
 
-int32_t	xI8ArrayPrint(const char * pHeader, uint8_t * pArray, int32_t ArraySize) {
-	int32_t iRV = 0 ;
-	if (pHeader)
-		iRV += printfx(pHeader) ;
-	for (int32_t Idx = 0; Idx < ArraySize; ++Idx)
-		iRV += printfx("  #%d=%d", Idx, pArray[Idx]) ;
-	iRV += printfx(strCRLF) ;
-	return iRV ;
+int xI8ArrayPrint(const char * pHeader, u8_t * pArray, int ArraySize) {
+	int iRV = 0;
+	if (pHeader) iRV += printfx(pHeader);
+	for (int Idx = 0; Idx < ArraySize; ++Idx)
+		iRV += printfx("  #%d=%d", Idx, pArray[Idx]);
+	iRV += printfx(strCRLF);
+	return iRV;
 }
